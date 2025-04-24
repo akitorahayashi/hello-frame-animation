41ed928438b22b01055e66cc26cd6c348c223816
akitorahayashi <atora.has75@gmail.com>
2025-04-24 13:05:24 +0900

    swiftlintとswiftformatを導入した

--- HelloFrameAnimation/ContentView.swift
+++ HelloFrameAnimation/ContentView.swift
@@ -18,17 +18,17 @@ struct ContentView: View {
                     style: StrokeStyle(lineWidth: DesignConstants.lineWidth, lineCap: .round, lineJoin: .round)
                 )
                 .frame(width: geometry.size.width, height: geometry.size.height)
-                .onChange(of: drawProgress) { oldValue, newValue in
-                    if newValue == AnimationConstants.endTrim && isAnimatingForward {
+                .onChange(of: drawProgress) { _, newValue in
+                    if newValue == AnimationConstants.endTrim, isAnimatingForward {
                         DispatchQueue.main.asyncAfter(deadline: .now() + AnimationConstants.animationDelay) {
                             isAnimatingForward = false
                             withAnimation(.easeInOut(duration: AnimationConstants.duration)) {
                                 drawProgress = AnimationConstants.startTrim
                             }
                         }
-                    } else if newValue == AnimationConstants.startTrim && !isAnimatingForward {
-                         isAnimatingForward = true
-                         isAnimating = false
+                    } else if newValue == AnimationConstants.startTrim, !isAnimatingForward {
+                        isAnimatingForward = true
+                        isAnimating = false
                     }
                 }
                 .contentShape(Rectangle())
--- HelloFrameAnimationUITests/HelloFrameAnimationUITests.swift
+++ HelloFrameAnimationUITests/HelloFrameAnimationUITests.swift
@@ -6,11 +6,10 @@ final class HelloFrameAnimationUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        
+
         app = XCUIApplication()
         app?.launch()
 
-        // 左横向きに回転
         XCUIDevice.shared.orientation = .landscapeRight
     }
 
@@ -22,14 +21,17 @@ final class HelloFrameAnimationUITests: XCTestCase {
 
     @MainActor
     func testHelloAnimationScreenshots() throws {
-
         // 保存先ディレクトリのパスを設定
         let screenshotsDirectory = URL(fileURLWithPath: #file)
             .deletingLastPathComponent() // HelloFrameAnimationUITests.swift
             .appendingPathComponent("ScreenShots")
 
         // ディレクトリが存在しない場合は作成
-        try? FileManager.default.createDirectory(at: screenshotsDirectory, withIntermediateDirectories: true, attributes: nil)
+        try? FileManager.default.createDirectory(
+            at: screenshotsDirectory,
+            withIntermediateDirectories: true,
+            attributes: nil
+        )
 
         // 最初のアニメーションが中間点に達するまで待機
         sleep(UInt32(AnimationConstants.duration + 0.5))
@@ -37,12 +39,16 @@ final class HelloFrameAnimationUITests: XCTestCase {
         // 最初のスクリーンショットを撮影
         let screenshot1 = XCUIScreen.main.screenshot()
         let attachment1 = XCTAttachment(screenshot: screenshot1)
-        attachment1.name = "Midpoint_Initial_LandscapeLeft"
+        attachment1.name = "Midpoint_Initial_LandscapeRight"
         attachment1.lifetime = .keepAlways
         add(attachment1)
 
         // スクリーンショットをファイルに保存
-        saveScreenshotAndAttachment(named: attachment1.name ?? "screenshot1", screenshot: screenshot1, in: screenshotsDirectory)
+        saveScreenshotAndAttachment(
+            named: attachment1.name ?? "screenshot1",
+            screenshot: screenshot1,
+            in: screenshotsDirectory
+        )
 
         sleep(UInt32(AnimationConstants.duration + AnimationConstants.animationDelay))
 
@@ -55,12 +61,16 @@ final class HelloFrameAnimationUITests: XCTestCase {
         // 2番目のスクリーンショットを撮影
         let screenshot2 = XCUIScreen.main.screenshot()
         let attachment2 = XCTAttachment(screenshot: screenshot2)
-        attachment2.name = "Midpoint_Restarted_LandscapeLeft"
+        attachment2.name = "Midpoint_Restarted_LandscapeRight"
         attachment2.lifetime = .keepAlways
         add(attachment2)
 
         // スクリーンショットをファイルに保存
-        saveScreenshotAndAttachment(named: attachment2.name ?? "screenshot2", screenshot: screenshot2, in: screenshotsDirectory)
+        saveScreenshotAndAttachment(
+            named: attachment2.name ?? "screenshot2",
+            screenshot: screenshot2,
+            in: screenshotsDirectory
+        )
     }
 
     private func saveScreenshotAndAttachment(named name: String, screenshot: XCUIScreenshot, in directory: URL) {
