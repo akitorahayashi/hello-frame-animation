53db3bc5a1516544b08f89adae1beb1fe55e6f86
akitorahayashi <atora.has75@gmail.com>
2025-04-24 11:09:30 +0900

    Initial commit

---
+++ HelloFrameAnimation/ContentView.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+
+struct ContentView: View {
+    private struct DesignConstants {
+        static let lineWidth: CGFloat = 3
+        static let gradientColors: [Color] = [.pink, .purple, .cyan]
+    }
+
+    private struct AnimationConstants {
+        static let duration: Double = 5.3
+        static let animationDelay: Double = 6.0
+        static let startTrim: CGFloat = 0.0
+        static let endTrim: CGFloat = 1.0
+    }
+
+    @State private var drawProgress: CGFloat = AnimationConstants.endTrim
+    @State private var isAnimatingForward = true
+
+    var body: some View {
+        GeometryReader { geometry in
+            HelloPath()
+                .trim(from: AnimationConstants.startTrim, to: drawProgress)
+                .stroke(
+                    LinearGradient(
+                        gradient: Gradient(colors: DesignConstants.gradientColors),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ),
+                    style: StrokeStyle(lineWidth: DesignConstants.lineWidth, lineCap: .round, lineJoin: .round)
+                )
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .onAppear {
+                    startAnimation()
+                }
+                .onChange(of: drawProgress) { oldValue, newValue in
+                    if newValue == AnimationConstants.endTrim && isAnimatingForward {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + AnimationConstants.animationDelay) {
+                            isAnimatingForward = false
+                            withAnimation(.easeInOut(duration: AnimationConstants.duration)) {
+                                drawProgress = AnimationConstants.startTrim
+                            }
+                        }
+                    } else if newValue == AnimationConstants.startTrim && !isAnimatingForward {
+                         isAnimatingForward = true
+                    }
+                }
+        }
+    }
+
+    // アニメーションを開始する関数
+    func startAnimation() {
+        if drawProgress == AnimationConstants.endTrim {
+           drawProgress = AnimationConstants.startTrim
+        }
+        isAnimatingForward = true
+        // アニメーションで描画を進める
+        withAnimation(.easeInOut(duration: AnimationConstants.duration)) {
+            drawProgress = AnimationConstants.endTrim
+        }
+    }
+}
---
+++ HelloFrameAnimation/HelloFrameAnimationApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct HelloFrameAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
